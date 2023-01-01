5676359b94a92ffa10f83d524cc0cf482ed09706
Baila3 <bailaly16@gmail.com>
2022-12-31 22:48:05 -0500

    create lookup form search

--- src/Components/LookUpForm/LookUpForm.jsx
+++ src/Components/LookUpForm/LookUpForm.jsx
@@ -1,51 +1,64 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './LookUpForm.scss'
 import axios from "axios";
+
 
 export default function LookUpForm() {
   const [name, setName] = useState(null);
   const [id, setId] = useState(null)
   const [department, setDepartment] = useState(null);
-  const [emplyoyed, setEmployed] = useState(null)
+  const [employed, setEmployed] = useState(null)
   const [email, setEmail] = useState (null)
+  const [posts, setPosts] = useState([]);
 
- async function onSubmit() {
-  await axios.get('http://localhost:3000/users', {name, email, id, department, emplyoyed})
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      })
-  }
+  useEffect(() => {
+    axios.get('http://localhost:3000/users')
+     .then(res => setPosts(res.data))
+     .catch(err => console.log(err));
+  }, [])
+
+   function filteredObj() {
+      return posts.filter(item =>  item.name === name ||  item.email  === email || item.id === id || item.department  === department || item.employed == employed ); 
+    }
 
   return(
-
-    <div className="signup_wrapper">
-        <div className='boxS'>
-        <h1>Employee Database</h1>
+    <div>
+      <h1 className="empData">Employee Database</h1>
+      <div className="Lookup_wrapper">
           <label>
-            <h4>Name:</h4>
+            <h4 className="height">Name:</h4>
             <input className='forms'  type="text" name="name" onChange={(ev) => setName(ev.target.value)}/>
           </label>
           <label>
-            <h4>Email:</h4>
+            <h4 className="height">Email:</h4>
             <input className='forms' type="text" name="email" onChange={(ev) => setEmail(ev.target.value)}/>
           </label>
           <label>
-            <h4>ID:</h4>
-            <input className='forms' type="text" name="id" onChange={(ev) => setId(ev.target.value)}/>
+            <h4 className="height">ID:</h4>
+            <input className='forms' type="text" name="id" onChange={(ev) => setId(Number(ev.target.value))}/>
           </label>
           <label>
-            <h4>Department:</h4>
+            <h4 className="height">Department:</h4>
             <input className='forms' type="text" name="department" onChange={(ev) => setDepartment(ev.target.value)}/>
           </label>
           <label>
-            <h4>Employment:</h4>
+            <h4 className="height">Employed:</h4>
             <input className='forms' type="text" name="employment" onChange={(ev) => setEmployed(ev.target.value)}/>
           </label>
-          <div>
-            <button className='user_btn' type="submit" onClick={onSubmit}>Register</button>
-          </div>
-      </div> 
+        </div>
+        <section>
+            {filteredObj().map(employee => {
+              return (
+                <li className="empBox" key={employee.id}>
+                <h2>{employee.name }</h2>
+                <p>Email: {employee.email}</p>
+                <p>ID: {employee.id}</p>
+                <p>Depatment: {employee.department}</p>
+                <p>Employed: {employee.employed}</p>
+                </li>
+              )
+            })}
+        </section>
     </div>
   );
 }
--- src/Components/RegisterForm/RegisterForm.jsx
+++ src/Components/RegisterForm/RegisterForm.jsx
@@ -1,44 +1,56 @@
 import React, {useState} from "react";
 import './RegisterForm.scss'
 import axios from "axios";
+import swal from 'sweetalert';
 
 export default function RegisterForm() {
   const [name, setName] = useState(null);
   const [department, setDepartment] = useState(null);
-  const [emplyoyed, setEmployed] = useState(null)
+  const [employed, setEmployed] = useState(null)
   const [email, setEmail] = useState (null)
+  const [selectedFile, setSelectedFile] = useState(null);
 
- async function onSubmit() {
-  await axios.post('http://localhost:3000/users', {name, email, department, emplyoyed})
+ async function onSubmit(e) {
+  e.preventDefault()
+  await axios.post('http://localhost:3000/users', {name, email, department, employed, selectedFile})
       .then(res => {
+        console.log(selectedFile)
         console.log(res);
         console.log(res.data);
       })
+      swal("Employee Sucessfully Registered")
+      setTimeout(() => {
+        window.location.reload(false);
+      }, 120000000);
   }
 
   return(
 
     <div className="signup_wrapper">
         <div className='boxS'>
-        <h1>Register</h1>
+        <h1>Register Employee</h1>
           <label>
-            <h4>Name:</h4>
+            <h3 className="height1">Name:</h3>
             <input className='forms'  type="text" name="name" onChange={(ev) => setName(ev.target.value)}/>
           </label>
           <label>
-            <h4>Email:</h4>
+            <h3 className="height1">Email:</h3>
             <input className='forms' type="text" name="email" onChange={(ev) => setEmail(ev.target.value)}/>
           </label>
           <label>
-            <h4>Department:</h4>
+            <h3 className="height1">Department:</h3>
             <input className='forms' type="text" name="department" onChange={(ev) => setDepartment(ev.target.value)}/>
           </label>
           <label>
-            <h4>Employment:</h4>
-            <input className='forms' type="text" name="employment" onChange={(ev) => setEmployed(ev.target.value)}/>
+            <h3 className="height1">Currently Employed:</h3>
+            <input className='forms' type="text" name="employment" onChange={(ev) => setEmployed(ev.target.value)} placeholder="Yes/No"/>
+          </label>
+          <label>
+            <h3 className="height1">Upload Avatar:</h3>
+            <input  className="file" type="file" onChange={(e) => setSelectedFile(e.target.files[0].name)} />
           </label>
           <div>
-            <button className='user_btn' type="submit" onClick={onSubmit}>Register</button>
+            <button className='user_btn' type="submit" onClick={onSubmit}><b>Register</b></button>
           </div>
       </div> 
     </div>
